12711e058666a0e55537b00e66917ac2bc729644
PapelRasgado <joaodantas31.jpd@gmail.com>
2018-10-22 16:14:04 -0300

    Final

--- Projeto_Hugo/src/main/java/bean/ProfessorBean.java
+++ Projeto_Hugo/src/main/java/bean/ProfessorBean.java
@@ -1,13 +1,20 @@
 package bean;
+import java.io.IOException;
 import java.io.Serializable;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.Principal;
 import java.util.Collection;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import entity.Professor;
 import service.ProfessorService;
@@ -38,6 +45,32 @@ public class ProfessorBean implements Serializable {
 		entidade = newEntidade();
 		entidades = getService().getAll();
 	}
+	
+	public String getUserLogin() {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		ExternalContext externalContext = facesContext.getExternalContext();
+		Principal userPrincipal = externalContext.getUserPrincipal();
+		if (userPrincipal == null) {
+			return "";
+		}
+		return userPrincipal.getName();
+	}
+	
+	public void efetuarLogout() throws IOException, ServletException {
+		FacesContext fc = FacesContext.getCurrentInstance();
+		ExternalContext ec = fc.getExternalContext();
+		HttpSession session = (HttpSession) ec.getSession(false);
+		session.invalidate();
+		HttpServletRequest request = (HttpServletRequest) ec.getRequest();
+		request.logout();
+		ec.redirect(ec.getApplicationContextPath());
+	}
+	
+	public boolean isUserInRole(String role) {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		ExternalContext externalContext = facesContext.getExternalContext();
+		return externalContext.isUserInRole(role);
+	}
 
 	public void remove(Professor entidade) {
 		getService().remove(entidade);
@@ -62,7 +95,7 @@ public class ProfessorBean implements Serializable {
 
 	public void save(){
 		try {
-			MessageDigest md = MessageDigest.getInstance("MD5");
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
 			md.update(senha.getBytes());
 			entidade.setHashSenha(md.digest());
 		} catch (NoSuchAlgorithmException e) {
